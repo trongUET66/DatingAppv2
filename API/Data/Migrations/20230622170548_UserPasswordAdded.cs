cd2acf5d25800eea3428d5bc369e152593edfdd5
trongUET66 <trong.uet.vnu@gmail.com>
2023-06-23 10:39:05 +0700

    End of section 4

---
+++ API/Data/Migrations/20230622170548_UserPasswordAdded.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace API.Data.Migrations
+{
+    public partial class UserPasswordAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PassWordHash",
+                table: "Users",
+                type: "BLOB",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PassWordSalt",
+                table: "Users",
+                type: "BLOB",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PassWordHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PassWordSalt",
+                table: "Users");
+        }
+    }
+}
--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -9,5 +9,7 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string UserName { get; set; }
+        public byte[] PassWordHash {get; set;}
+        public byte[] PassWordSalt { get; set; }
     }
 }
