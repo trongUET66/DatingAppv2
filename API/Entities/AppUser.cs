c1dd48d4ccc10bdb2fd6bda48ed863a38d4e37e6
trongUET66 <trong.uet.vnu@gmail.com>
2023-07-05 14:41:04 +0700

    end of section 8

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -2,14 +2,31 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Extensions;
 
 namespace API.Entities
 {
     public class AppUser
     {
+        
         public int Id { get; set; }
         public string UserName { get; set; }
         public byte[] PassWordHash {get; set;}
         public byte[] PassWordSalt { get; set; }
+        public DateTime DateOfBirth { get; set; }   
+        public string KnowAs { get; set; }
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime LastActive { get; set; } = DateTime.Now;
+        public string Gender { get; set; } 
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interets { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public ICollection<Photo> Photos { get; set; } 
+        // public int GetAge()
+        // {
+        //     return DateOfBirth.CaculateAge();
+        // }
     }
 }
